8e30478c703978c7816d09710d70aa45bbd4e71f
cmiller241 <99040885+cmiller241@users.noreply.github.com>
2023-10-27 07:06:59 -0800

    Updated loadhandler to include map loading. Updated window resizing so game fits entire window regardless of size.

--- js/Map.js
+++ js/Map.js
@@ -6,23 +6,34 @@ class Map {
         this.mapArray = null;
     }
 
-    load() {
- 
-
-        const existingScript = document.getElementById('mapScript');
-        if (existingScript) {
-            document.head.removeChild(existingScript);
-        }
-
-        const script = document.createElement('script');
-        script.src = `/${this.mapFileName}`;
-        script.id = 'mapScript';
-        script.onload = () => {
+    async load() {
+        try {
+            await this.loadMapScript();
             const mapArray = window.mapArray;
             if (mapArray) {
                 this.mapArray = mapArray;
+                console.log("THe map array is loaded");
+            } else {
+                console.log("The map array is null");
             }
+            return Promise.resolve();
+        } catch {
+            console.error("Error loading map:", error);
+            return Promise.reject(error);
         }
-        document.head.appendChild(script);    
+    }
+
+    loadMapScript() {
+        return new Promise((resolve, reject) => {
+            const existingScript = document.getElementById('mapScript');
+            if (existingScript) {
+                document.head.removeChild(existingScript);
+            }
+            const script = document.createElement('script');
+            script.src = `/${this.mapFileName}`;
+            script.onload = resolve;
+            script.onerror = reject;
+            document.head.appendChild(script);
+        });
     }
 }
